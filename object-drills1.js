d56ee3a6ef71f2eca35d55cf964096542774d2b9
Mehul Patel <mehulpatel118@gmail.com>
2019-02-21 11:33:31 -0500

    object drill 1

---
+++ object-drills1.js
@@ -0,0 +1,15 @@
+'use strict';
+// Object Drill 1
+
+const loaf = {
+  flour:300,
+  water:210,
+  hydration: function(){
+    return this.water/this.flour * 100;
+  }
+
+};
+
+console.log(loaf.flour);
+console.log(loaf.water);
+console.log(loaf.hydration());
